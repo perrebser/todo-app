d8d7cdaa45365c8e3bbe838a795cef5d7fb8a576
perrebser <workperrebser@gmail.com>
2023-10-26 18:36:35 +0200

    handle add todo

--- src/App.tsx
+++ src/App.tsx
@@ -11,32 +11,60 @@ const mockTodos = [
 
 const App = () => {
   const [todos, setTodos] = useState(mockTodos);
-
+  const [state, setState] = useState("");
+  
   const handleRemoveTodo = (id: number): void => {
     const newTodos = todos.filter((todo) => todo.id !== id);
     setTodos(newTodos);
   };
 
-  const handleCompleted= ({id, completed}:Pick<TodoType,'id' | 'completed'>):void =>{
-    console.log(completed)
-      const newTodos=todos.map(todo=>{
-        if(todo.id===id){
-          return {
-            ...todo,
-            completed: !completed
-          }
-        }
-        return todo
-      })
-      // console.log(newTodos)
-      setTodos(newTodos)
+  const handleCompleted = ({
+    id,
+    completed,
+  }: Pick<TodoType, "id" | "completed">): void => {
+    const newTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          completed: !completed,
+        };
+      }
+      return todo;
+    });
+    // console.log(newTodos)
+    setTodos(newTodos);
+  };
+
+  const handleAddTodo = (title: string): void => {
+    const lastIndex: number = todos[todos.length - 1].id;
+    const newTodo = {
+      id: lastIndex + 1,
+      title: title,
+      completed: false,
+    };
+    const newTodos = todos.concat(newTodo);
+    setTodos(newTodos);
+  };
+
+  const handleChange = (e:React.ChangeEvent<HTMLInputElement>): void => {
+    setState(e.target.value)
+  };
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) =>{
+    if (e.keyCode === 13) {
+      if (state=== "") return;
+      handleAddTodo(state)
+    }
   }
   return (
     <>
       <h1 className="text-center font-sans text-4xl	font-bold py-5">Todo App</h1>
-      <HeaderTodo/>
+      <HeaderTodo onHandleInputChange={handleChange} onHandleKeyDown={handleKeyDown} />
       <div>
-        <Todos todos={todos} onRemoveTodo={handleRemoveTodo} onToggleCompleted={handleCompleted} />
+        <Todos
+          todos={todos}
+          onRemoveTodo={handleRemoveTodo}
+          onToggleCompleted={handleCompleted}
+        />
       </div>
     </>
   );
--- src/components/HeaderTodo.tsx
+++ src/components/HeaderTodo.tsx
@@ -1,15 +1,17 @@
 import { Button, Input } from "@chakra-ui/react";
 import { ListTodoType,TodoType } from "../types";
+import React from 'react';
 
+interface Props{
+  onHandleInputChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onHandleKeyDown: (e: React.KeyboardEvent<HTMLInputElement>) => void;
+}
 
-  const handleAddTodo= () =>{
-    
-  }
-const HeaderTodo: React.FC = () => {
+const HeaderTodo: React.FC<Props> = ({onHandleInputChange,onHandleKeyDown}) => {
   return (
     <div className="w-2/3 justify-around flex">
-      <Input placeholder="Basic usage" autoFocus={true} onChange={()=>{}}/>
-      <Button colorScheme='blue' onClick={handleAddTodo}>Add Todo</Button>
+      <Input placeholder="Write Todo..." autoFocus={true} onKeyDown={(e)=>{onHandleKeyDown(e)}} onChange={(e)=>{onHandleInputChange(e)}}/>
+      <Button colorScheme='blue' onClick={()=>{}}>Add Todo</Button>
     </div>
   );
 };
